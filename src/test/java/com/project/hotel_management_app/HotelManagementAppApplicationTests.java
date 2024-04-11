e7718dfc964a6b2935edf017d47e12b5df4298f3
meddourisami <sami.meddouri@etudiant-fst.utm.tn>
2024-04-11 02:17:25 +0200

    Initial commit

---
+++ src/test/java/com/project/hotel_management_app/HotelManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.hotel_management_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
